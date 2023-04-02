8f413b3dfb33ccf111084fb6f4806dd3febcb4eb
eliannapot <up1072682@upnet.gr>
2023-04-02 13:20:10 +0300

    made sticky navbar added google maps feature that needs API KEY

---
+++ project.js
@@ -0,0 +1,9 @@
+
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+      center: {lat: 38.238558920524724, lng: 21.734261025667106},
+      zoom: 12
+    });
+  }
+  
