d4c37a0f6c1c7180330aee650f2d3217385db0a0
eliannapot <up1072682@upnet.gr>
2023-04-26 21:39:24 +0300

    small changes

--- modal.js
+++ modal.js
@@ -38,80 +38,81 @@ modal.innerHTML = `<div class="modal-dialog modal-dialog-centered" role="documen
                     </div>
                     </div>`;
 
-                    const username = document.getElementById('sign_up_username');
-                    const email = document.getElementById('sign_up_email');
-                    const password = document.getElementById('sign_up_password');  
-                    const passwordConfirm = document.getElementById('sign_up_password_repeat');
+const username = document.getElementById('sign_up_username');
+const email = document.getElementById('sign_up_email');
+const password = document.getElementById('sign_up_password');  
+const passwordConfirm = document.getElementById('sign_up_password_repeat');
 
-                    const errorElement = document.getElementById('errorMessage');
-                    const successElement = document.getElementById('successMessage');
-                    //button.addEventListener("click", Validation);
-                    email.addEventListener("input", Validation);
-                    username.addEventListener("input", Validation);
-                    password.addEventListener("input", Validation);
-                    passwordConfirm.addEventListener("input", Validation);
-                    function Validation() {
-                         
-                      let messages = [];
+const errorElement = document.getElementById('errorMessage');
+const successElement = document.getElementById('successMessage');
+//button.addEventListener("click", Validation);
+email.addEventListener("input", Validation);
+username.addEventListener("input", Validation);
+password.addEventListener("input", Validation);
+passwordConfirm.addEventListener("input", Validation);
+
+function Validation() {
       
-                      //if all fields are filled
-                      if(!mandatoryTextCheck()) {
-                          messages.push('All fields are required');
-                      }
-      
-                      //checks if the email is valid
-                      var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/;
-                      if (!validateInput(email, emailRegex)) {
-                          messages.push('Email is not valid');
-                      }
-      
-                      //checks if the username  is valid
-                      var usernameRegex = /^.{6,20}$/
-                      if (!validateInput(username, usernameRegex)) {
-                          messages.push('Username should be in between 6 and 20 characters.');
-                      }
-      
-                      //if passwords match
-                      if (password.value !== passwordConfirm.value) {
-                          messages.push("Passwords do not match.");
-                      }
-      
-                      //if password is valid
-                      var passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/;
-                      if (!validateInput(password, passwordRegex) && !validateInput(passwordConfirm, passwordRegex) ) {
-                          messages.push('Password must be between 6 and 20 characters and must have at least one letter and one number');
-                      }
-      
-                      if (messages.length > 0) {
-                          errorElement.innerText = messages.join(', ');
-                          successElement.style.display = "none";
-                      } else {
-                          successElement.style.display = "block";
-                          errorElement.style.display = "none";
-                      }
-                  }
-      
-                  //All fields are filled in check
-                  //returns true if theyre all good
-                  function mandatoryTextCheck() {
-                      if (email.value === '' || email.value == null || 
-                      username.value === '' || username.value == null ||
-                      password.value === '' || password.value == null ||
-                      passwordConfirm.value === '' || passwordConfirm.value == null) {
-                          return false;
-                      }
-                      return true;
-                  };
-      
-                  //Validate input
-                  function validateInput(inputElement, regex) {
-                      const isValid = regex.test(inputElement.value);
-      
-                      if (isValid) {
-                          inputElement.style.color = 'green';
-                          return true;
-                      } else {
-                          inputElement.style.color = 'red';
-                          return false;
-                      }
-                  }
+  let messages = [];
+
+  //if all fields are filled
+  if(!mandatoryTextCheck()) {
+      messages.push('All fields are required');
+  }
+
+  //checks if the email is valid
+  var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/;
+  if (!validateInput(email, emailRegex)) {
+      messages.push('Email is not valid');
+  }
+
+  //checks if the username  is valid
+  var usernameRegex = /^.{6,20}$/
+  if (!validateInput(username, usernameRegex)) {
+      messages.push('Username should be in between 6 and 20 characters.');
+  }
+
+  //if passwords match
+  if (password.value !== passwordConfirm.value) {
+      messages.push("Passwords do not match.");
+  }
+
+  //if password is valid
+  var passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/;
+  if (!validateInput(password, passwordRegex) || !validateInput(passwordConfirm, passwordRegex) ) {
+      messages.push('Password must be between 6 and 20 characters and must have at least one letter and one number');
+  }
+
+  if (messages.length > 0) {
+      errorElement.innerText = messages.join(', ');
+      successElement.style.display = "none";
+  } else {
+      successElement.style.display = "block";
+      errorElement.style.display = "none";
+  }
+}
+
+//All fields are filled in check
+//returns true if theyre all good
+function mandatoryTextCheck() {
+  if (email.value === '' || email.value == null || 
+  username.value === '' || username.value == null ||
+  password.value === '' || password.value == null ||
+  passwordConfirm.value === '' || passwordConfirm.value == null) {
+      return false;
+  }
+  return true;
+};
+
+//Validate input
+function validateInput(inputElement, regex) {
+  const isValid = regex.test(inputElement.value);
+
+  if (isValid) {
+      inputElement.style.color = 'green';
+      return true;
+  } else {
+      inputElement.style.color = 'red';
+      return false;
+  }
+}
